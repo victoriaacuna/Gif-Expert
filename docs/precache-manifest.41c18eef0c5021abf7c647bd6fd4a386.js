04b99d96e03a13930317058cc0afbc81820c9cdf
Victoria Acuna <victoria.ines.11@gmail.com>
2020-09-21 01:46:50 -0400

    first commit

---
+++ docs/precache-manifest.41c18eef0c5021abf7c647bd6fd4a386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1273e78b8c33ae8d534d796cf91a73dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87ec547ae837d47087c",
+    "url": "/static/css/main.ff3aa80c.chunk.css"
+  },
+  {
+    "revision": "5706df19e336187cb788",
+    "url": "/static/js/2.a2ef18de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2ef18de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87ec547ae837d47087c",
+    "url": "/static/js/main.5bf47f89.chunk.js"
+  },
+  {
+    "revision": "903e7817fc2a18360c09",
+    "url": "/static/js/runtime-main.5142bbd6.js"
+  }
+]);
